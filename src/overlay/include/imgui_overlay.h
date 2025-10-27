3b69aae5a6394256f42fa001057bc00c5c51cf19
Nuno Silva <nuno.levezinho@live.com.pt>
2025-10-27 22:54:55 +0000

    Test passing render data to imgui overlay

--- src/overlay/include/imgui_overlay.h
+++ src/overlay/include/imgui_overlay.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include "lib.h"
+#include <lib.h>
+#include <render.h>
 #include <Windows.h>
 
 extern "C"
 {
     EXPORT_FN void overlay_setup(void* platform_handle, void* device_context);
-    EXPORT_FN void overlay_render();
+    EXPORT_FN void overlay_render(RenderData* renderData);
     EXPORT_FN void overlay_shutdown();
     EXPORT_FN BOOL overlay_handle_wndproc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 }
--- src/runtime/src/renderer.h
+++ src/runtime/src/renderer.h
@@ -6,7 +6,7 @@ struct BumpAllocator;
 struct RenderData;
 
 // declare PFN to UI Overlay render function
-typedef void(*pfnRenderUIOverlay)();
+typedef void(*pfnRenderUIOverlay)(RenderData* renderData);
 
 void renderer_init(int width, int height, void* handle, BumpAllocator* persistentStorage);
 void renderer_shutdown();
