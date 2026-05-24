df8dd1e474b2385e8d09a09499bc1c9e2f6ab3ff
Nuno Silva <nuno.levezinho@live.com.pt>
2026-05-24 20:36:25 +0100

    fix: _log formats once via format_log_line; drop off-by-one reset vararg (WARN/ERROR/assert now correct)

--- src/common/include/lib.h
+++ src/common/include/lib.h
@@ -17,6 +17,8 @@
 #include <print>
 #include <ostream>
 
+#include "LogFormat.h"
+
 enum class RendererAPI : uint8_t {
     Invalid,
     DirectX12,
@@ -81,61 +83,17 @@ struct DebugCycleCounter
 //                           Logging
 // #############################################################################
 // Aparently works on Windows, Linux and Mac
-enum TextColor
-{
-  TEXT_COLOR_BLACK,
-  TEXT_COLOR_RED,
-  TEXT_COLOR_GREEN,
-  TEXT_COLOR_YELLOW,
-  TEXT_COLOR_BLUE,
-  TEXT_COLOR_MAGENTA,
-  TEXT_COLOR_CYAN,
-  TEXT_COLOR_WHITE,
-  TEXT_COLOR_BRIGHT_BLACK,
-  TEXT_COLOR_BRIGHT_RED,
-  TEXT_COLOR_BRIGHT_GREEN,
-  TEXT_COLOR_BRIGHT_YELLOW,
-  TEXT_COLOR_BRIGHT_BLUE,
-  TEXT_COLOR_BRIGHT_MAGENTA,
-  TEXT_COLOR_BRIGHT_CYAN,
-  TEXT_COLOR_BRIGHT_WHITE,
-  TEXT_COLOR_COUNT
-};
+// TextColor / color table now live in LogFormat.h.
 
 template <typename... Args>
-void _log(const char* prefix, const char* msg, TextColor textColor, Args... args)
+void _log(const char* prefix, const char* msg, TextColor color, Args... args)
 {
-  static const char* TextColorTable [TEXT_COLOR_COUNT] =
-  {
-    "\x1b[30m", // TEXT_COLOR_BLACK
-    "\x1b[31m", // TEXT_COLOR_RED
-    "\x1b[32m", // TEXT_COLOR_GREEN
-    "\x1b[33m", // TEXT_COLOR_YELLOW
-    "\x1b[34m", // TEXT_COLOR_BLUE
-    "\x1b[35m", // TEXT_COLOR_MAGENTA
-    "\x1b[36m", // TEXT_COLOR_CYAN
-    "\x1b[37m", // TEXT_COLOR_WHITE
-    "\x1b[90m", // TEXT_COLOR_BRIGHT_BLACK
-    "\x1b[91m", // TEXT_COLOR_BRIGHT_RED
-    "\x1b[92m", // TEXT_COLOR_BRIGHT_GREEN
-    "\x1b[93m", // TEXT_COLOR_BRIGHT_YELLOW
-    "\x1b[94m", // TEXT_COLOR_BRIGHT_BLUE
-    "\x1b[95m", // TEXT_COLOR_BRIGHT_MAGENTA
-    "\x1b[96m", // TEXT_COLOR_BRIGHT_CYAN
-    "\x1b[97m", // TEXT_COLOR_BRIGHT_WHITE
-  };
-
-  char formatBuffer[8192] = {};
-  sprintf(formatBuffer, "%s %s %s \033[0m",TextColorTable[textColor], prefix, msg);
-
-  static char buffer[8192] = {};
-  sprintf(buffer, formatBuffer, args...);
-  std::println("{}", buffer);
+    std::println("{}", format_log_line(prefix, msg, color, args...));
 }
 
 #define SM_TRACE(msg, ...) do { _log("TRACE:", msg, TEXT_COLOR_GREEN, ##__VA_ARGS__); } while(0)
-#define SM_WARN(msg, ...)  do { _log("WARN:",  msg, TEXT_COLOR_YELLOW, "\033[0m", ##__VA_ARGS__); } while(0)
-#define SM_ERROR(msg, ...) do { _log("ERROR:", msg, TEXT_COLOR_RED, "\033[0m", ##__VA_ARGS__); } while(0)
+#define SM_WARN(msg, ...)  do { _log("WARN:",  msg, TEXT_COLOR_YELLOW, ##__VA_ARGS__); } while(0)
+#define SM_ERROR(msg, ...) do { _log("ERROR:", msg, TEXT_COLOR_RED,    ##__VA_ARGS__); } while(0)
 
 // Forward-declare a platform-specific debug break that can show dialogs, etc.
 void platform_debug_break(const char* expr, const char* file, int line, const char* message);
@@ -146,9 +104,9 @@ inline void sm_assert_fail(const char* expr, const char* file, int line, const c
 {
   // Format the message once for both logging and platform handler
   char formatted[1024] = {};
-  sprintf(formatted, fmt, args...);
+  std::snprintf(formatted, sizeof(formatted), fmt, args...);
   // Log through our normal logger
-  _log("ERROR:", "%s", TEXT_COLOR_RED, "\033[0m", formatted);
+  _log("ERROR:", "%s", TEXT_COLOR_RED, formatted);
   // Delegate to platform to decide how to break/notify
   platform_debug_break(expr, file, line, formatted);
 }
